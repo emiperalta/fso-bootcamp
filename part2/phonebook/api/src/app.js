2e4539d49d81682521bbd2775a4271cca8f8c5f6
EmiPeralta <emilianop96@hotmail.com>
2021-03-30 02:24:05 -0300

    ex 3.7 - 3.8: morgan middleware added for logs

--- part2/phonebook/api/src/app.js
+++ part2/phonebook/api/src/app.js
@@ -1,12 +1,14 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const personRoutes = require('./routes/person.routes');
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
